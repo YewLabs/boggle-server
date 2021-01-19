11dae93b0b28cc605bb6aca5d325e70a7146d51d
krawthekrow <krawthekrow@gmail.com>
2021-01-19 09:53:09 -0500

    sync upstream changes

--- models.py
+++ models.py
@@ -5,7 +5,16 @@ class BoggleTeamData(models.Model):
     team = models.OneToOneField(Team, on_delete=models.CASCADE)
     world = models.TextField()
 
+    def __str__(self):
+        return '%s' % (self.team)
+
 class BoggleHighScoreData(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     level = models.PositiveIntegerField(default=0)
     score = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return '%s (%d: %d)' % (self.team, self.level, self.score)
+
+    class Meta:
+        unique_together = ('team', 'level')
